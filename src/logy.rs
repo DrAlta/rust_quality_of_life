07290d94aea013927be00b9ad556419a177e8e02
Hsingai Altaica <draltaica@yahoo.com>
2023-12-12 07:40:03 -0800

    impoves the logy!() macro so that it doesn't print the full path for filename when used in a dependancy

--- src/logy.rs
+++ src/logy.rs
@@ -1,7 +1,21 @@
+
 #[macro_export]
 macro_rules! logy {
     ($lvl:expr, $($arg:tt)*) => {
         #[cfg(feature = $lvl)]
-        println!("[{}:{}:{}]{}", $lvl, file!(), line!(), format!($($arg)*));
+        println!("[{}:{}{}:{}]{}", 
+        $lvl, 
+        env!("CARGO_CRATE_NAME"), 
+        {
+            if file!().starts_with(env!("CARGO_MANIFEST_DIR")) {
+                let x = env!("CARGO_MANIFEST_DIR").len();
+                &(file!()[x..])
+            } else {
+                concat!("/", file!())
+            }
+        }, 
+        line!(), 
+        format!($($arg)*)
+    )
     };
 }
