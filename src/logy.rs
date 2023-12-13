20b23968d43dbcc389627d92ad40c9daf14d4a73
Hsingai Altaica <draltaica@yahoo.com>
2023-12-13 12:17:42 -0800

    improved logy!()

--- src/logy.rs
+++ src/logy.rs
@@ -3,19 +3,24 @@
 macro_rules! logy {
     ($lvl:expr, $($arg:tt)*) => {
         #[cfg(feature = $lvl)]
-        println!("[{}:{}{}:{}]{}", 
-        $lvl, 
-        env!("CARGO_CRATE_NAME"), 
-        {
-            if file!().starts_with(env!("CARGO_MANIFEST_DIR")) {
-                let x = env!("CARGO_MANIFEST_DIR").len();
-                &(file!()[x..])
-            } else {
-                concat!("/", file!())
-            }
-        }, 
-        line!(), 
-        format!($($arg)*)
-    )
+        if file!().starts_with(env!("CARGO_MANIFEST_DIR")) {
+            println!("[{}:{}:{}:{}]{}",
+                $lvl,
+                env!("CARGO_CRATE_NAME"), 
+                {
+                    const x: usize = env!("CARGO_MANIFEST_DIR").len();
+                    &(file!()[x..])
+                },
+                line!(), 
+                format!($($arg)*)
+            )
+        } else {
+            println!("[{}:{}:{}]{}",
+                $lvl,
+                file!(),
+                line!(), 
+                format!($($arg)*)
+            )
+        }
     };
 }
